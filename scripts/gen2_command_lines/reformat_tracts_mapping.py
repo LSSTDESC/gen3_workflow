af65737307771b43c817aed314f73bb7340c5b12
Jim Chiang <jchiang@slac.stanford.edu>
2020-06-15 09:06:22 -0700

    add some documentation and use full path to input tract mapping db file

--- scripts/gen2_command_lines/reformat_tracts_mapping.py
+++ scripts/gen2_command_lines/reformat_tracts_mapping.py
@@ -1,7 +1,17 @@
+"""
+Script to reformat the tracts_mapping.sqlite3 file into a pandas dataframe
+where patch values are formatted more conveniently, e.g., '0,0' instead of
+'(0, 0)'.  Entries are limited to y01 visits for DR2 processing.
+"""
+import os
 import sqlite3
 import pandas as pd
 
-with sqlite3.connect('tracts_mapping.sqlite3') as conn:
+parent_repo = ('/global/cfs/cdirs/lsst/production/DC2_ImSim/Run2.2i'
+               '/desc_dm_drp/v19.0.0-v1/rerun/run2.2i-calexp-v1')
+tracts_mapping_db = os.path.join(parent_repo, 'tracts_mapping.sqlite3')
+
+with sqlite3.connect(tracts_mapping_db) as conn:
     df = pd.read_sql('select * from overlaps where visit <= 262078', conn)
 
 df['patch'] = ['{},{}'.format(*eval(_)) for _ in df['patch']]
