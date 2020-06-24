ae30b49dc4ee8d806d5426fa17a38563272cfb34
Jim Chiang <jchiang@slac.stanford.edu>
2020-06-23 22:17:26 -0700

    check merge coadd measurement outputs in compiling list of pipe tasks to run

--- scripts/gen2_command_lines/merge_coadd_measurements.py
+++ scripts/gen2_command_lines/merge_coadd_measurements.py
@@ -2,7 +2,8 @@ import os
 import sys
 import subprocess
 import multiprocessing
-from desc.gen3_workflow import CommandLineGenerator, dispatch_commands
+from desc.gen3_workflow import CommandLineGenerator, dispatch_commands,\
+    Gen2Products, missing_outputs
 
 sensor_visit_skymap_file = 'tracts_mapping_y01.pkl'
 parent_repo = ('/global/cfs/cdirs/lsst/production/DC2_ImSim/Run2.2i'
@@ -15,15 +16,22 @@ cl_gen = CommandLineGenerator(sensor_visit_skymap_file, parent_repo,
                               local_repo, bands=bands, log_dir=log_dir)
 
 rerun = 'coaddPhot'
-tract = 3828
-patches = sorted(list(set(cl_gen.df0.query(f'tract=={tract}')['patch'])))
+tract = 4849
+#patches = sorted(list(set(cl_gen.df0.query(f'tract=={tract}')['patch'])))
+patches = '0,0 0,1 0,2 0,3 0,4 0,5 0,6 1,0 1,1 1,2 1,3 1,4 1,5'.split()
+
+gen2_products = Gen2Products(os.path.join(local_repo, 'rerun', 'coaddPhot'))
 
 commands = []
 for patch in patches:
-    commands.append(
-        cl_gen.get_merge_task_cl('mergeCoaddMeasurements', rerun, tract, patch))
+    if missing_outputs(
+            gen2_products.merge_task_outputs('mergeCoaddMeasurements',
+                                             tract, patch)):
+        commands.append(
+            cl_gen.get_merge_task_cl('mergeCoaddMeasurements', rerun,
+                                     tract, patch))
 
 print(len(commands))
-processes = 32
+processes = 5
 dry_run = True
 dispatch_commands(commands, processes=processes, dry_run=dry_run)
