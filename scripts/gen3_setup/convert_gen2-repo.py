e9f2ac652272a9407dfec94a7f9e3f313d866eba
James Chiang <jchiang87@gmail.com>
2020-10-20 13:09:28 -0700

    move ingestRef code to convert_gen2-repo.py

--- scripts/gen3_setup/convert_gen2-repo.py
+++ scripts/gen3_setup/convert_gen2-repo.py
@@ -1,18 +1,15 @@
-from lsst.obs.base.gen2to3 import ConvertRepoTask
-from lsst.obs.base import Instrument
+from lsst.daf.butler import Butler, DatasetType, FileDataset
+from lsst.obs.base.gen2to3 import ConvertRepoTask, RootRepoConverter
 from lsst.obs.lsst import LsstCamImSim
-from lsst.daf.butler import Butler
 
-def makeTask(butler: Butler, *,  fresh_start: bool = True):
+
+def makeRawCalibConvertTask(butler: Butler, fresh_start: bool=True):
     instrument = LsstCamImSim()
     config = ConvertRepoTask.ConfigClass()
     instrument.applyConfigOverrides(ConvertRepoTask._DefaultName, config)
-    config.refCats = ["cal_ref_cat"]
     config.relatedOnly = True
-    #config.transfer = "symlink"
     config.transfer = "auto"
     config.datasetIncludePatterns = ["flat", "bias", "dark", "fringe", "SKY",
-#                                     "ref_cat",
                                      "raw"]
     config.datasetIgnorePatterns.append("*_camera")
     config.fileIgnorePatterns.extend(["*.log", "*.png", "rerun*"])
@@ -20,14 +17,40 @@ def makeTask(butler: Butler, *,  fresh_start: bool = True):
     config.doWriteCuratedCalibrations = fresh_start
     return ConvertRepoTask(config=config, butler3=butler, instrument=instrument)
 
+
+def makeRefCatConvertTask(butler: Butler):
+    instrument = LsstCamImSim()
+    config = ConvertRepoTask.ConfigClass()
+    instrument.applyConfigOverrides(ConvertRepoTask._DefaultName, config)
+    config.refCats = ["cal_ref_cat"]
+    config.relatedOnly = True
+    config.transfer = "symlink"
+    config.datasetIncludePatterns = ["ref_cat"]
+    return ConvertRepoTask(config=config, butler3=butler, instrument=instrument)
+
+
 if __name__ == '__main__':
     instrument = LsstCamImSim()
     root2 = 'gen2-repo'
-    root3 = 'gen3-repo'
+    root3 = 'gen3-repo-test'
+
+    # Convert raw files and CALIBs
     fresh_start = True
     butler = Butler(root3, run=instrument.makeDefaultRawIngestRunName())
-    task = makeTask(butler, fresh_start=fresh_start)
-    task.run(root=root2,
-             reruns=[],
+    task = makeRawCalibConvertTask(butler, fresh_start=fresh_start)
+    task.run(root=root2, reruns=[],
              calibs=({"CALIB": instrument.makeCollectionName("calib")}
                      if fresh_start else None))
+
+    # Convert ref_cats
+    butler = Butler(root3, run='ref_cat')
+    task = makeRefCatConvertTask(butler)
+    rootRepoConverter = RootRepoConverter(task=task, root=root2,
+                                          instrument=instrument)
+    rootRepoConverter.prep()
+    listOfFileDataset = list(rootRepoConverter.iterDatasets())
+    datasetType = DatasetType(name='cal_ref_cat', dimensions=['htm7'],
+                              storageClass='SimpleCatalog',
+                              universe=butler.registry.dimensions)
+    butler.registry.registerDatasetType(datasetType)
+    butler.ingest(*listOfFileDataset)
