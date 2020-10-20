36d914d46eceb22e076d055535eb2071ade219a8
James Chiang <jchiang87@gmail.com>
2020-10-19 18:50:47 -0700

    w_2020_42 update: use LsstCamImSim

--- scripts/gen3_setup/convert_gen2-repo.py
+++ scripts/gen3_setup/convert_gen2-repo.py
@@ -1,31 +1,33 @@
 from lsst.obs.base.gen2to3 import ConvertRepoTask
 from lsst.obs.base import Instrument
-from lsst.obs.lsst import LsstImSim
+from lsst.obs.lsst import LsstCamImSim
 from lsst.daf.butler import Butler
 
-def makeTask(butler: Butler, *, continue_: bool = False):
-    instrument = LsstImSim()
+def makeTask(butler: Butler, *,  fresh_start: bool = True):
+    instrument = LsstCamImSim()
     config = ConvertRepoTask.ConfigClass()
     instrument.applyConfigOverrides(ConvertRepoTask._DefaultName, config)
-    #config.instrument = "lsst.obs.lsst.LsstImSim"
+    config.refCats = ["cal_ref_cat"]
     config.relatedOnly = True
     #config.transfer = "symlink"
     config.transfer = "auto"
     config.datasetIncludePatterns = ["flat", "bias", "dark", "fringe", "SKY",
-                                     "ref_cat", "raw"]
+#                                     "ref_cat",
+                                     "raw"]
     config.datasetIgnorePatterns.append("*_camera")
     config.fileIgnorePatterns.extend(["*.log", "*.png", "rerun*"])
-    config.doRegisterInstrument = not continue_
-    config.doWriteCuratedCalibrations = not continue_
+    config.doRegisterInstrument = fresh_start
+    config.doWriteCuratedCalibrations = fresh_start
     return ConvertRepoTask(config=config, butler3=butler, instrument=instrument)
 
 if __name__ == '__main__':
-    instrument = LsstImSim()
-    root = 'gen3-repo'
-    continue_ = False
-    butler = Butler(root, run=instrument.makeDefaultRawIngestRunName())
-    task = makeTask(butler, continue_=continue_)
-    task.run(root='gen2-repo',
+    instrument = LsstCamImSim()
+    root2 = 'gen2-repo'
+    root3 = 'gen3-repo'
+    fresh_start = True
+    butler = Butler(root3, run=instrument.makeDefaultRawIngestRunName())
+    task = makeTask(butler, fresh_start=fresh_start)
+    task.run(root=root2,
              reruns=[],
              calibs=({"CALIB": instrument.makeCollectionName("calib")}
-                     if not continue_ else None))
+                     if fresh_start else None))
