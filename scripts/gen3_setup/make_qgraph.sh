4593ad163ef42dece8ded6f306e6a1b2b277e476
Jim Chiang <jchiang@slac.stanford.edu>
2020-10-20 22:31:30 -0700

    add tract, patch to output filenames; correct input collections to use LSSTCam-imSim instrument name

--- scripts/gen3_setup/make_qgraph.sh
+++ scripts/gen3_setup/make_qgraph.sh
@@ -1,15 +1,17 @@
+tract=3828
+patch=24
 #pipeline=ProcessCcd
 #pipeline=Coaddition
 pipeline=DRP
 
 pipetask qgraph \
    -d "tract=3828 AND patch=24" \
-   -i "LSST-ImSim/raw/all,LSST-ImSim/calib,refcats,skymaps/imsim" \
+   -i "LSSTCam-imSim/raw/all,LSSTCam-imSim/calib,ref_cat,skymaps/imsim" \
    -b gen3-repo/butler.yaml \
    --skip-existing \
    --instrument lsst.obs.lsst.LsstCamImSim \
    --pipeline ${GEN3_WORKFLOW_DIR}/pipelines/${pipeline}.yaml \
-   --save-qgraph ${pipeline}.pickle \
-   --qgraph-dot ${pipeline}.dot
+   --save-qgraph ${pipeline}_${tract}_${patch}.pickle \
+   --qgraph-dot ${pipeline}_${tract}_${patch}.dot
 
 #   -d "abstract_filter='i' and tract=3828 and patch=24 and detector=177" \
