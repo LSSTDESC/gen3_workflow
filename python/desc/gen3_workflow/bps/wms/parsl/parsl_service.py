8e603c8cfdc772ac87b1507bc6c11154e0e949aa
James Chiang <jchiang87@gmail.com>
2020-11-15 09:01:12 -0800

    refactor bash_app for running command lines; add doctrings and address other linting complaints

--- python/desc/gen3_workflow/bps/wms/parsl/parsl_service.py
+++ python/desc/gen3_workflow/bps/wms/parsl/parsl_service.py
@@ -3,22 +3,26 @@ Parsl-based workflow management service plug-in for ctrl_bps.
 """
 import os
 import subprocess
-import parsl
 import lsst.utils
 from lsst.daf.butler import Butler
 from lsst.ctrl.bps.wms_service import BaseWmsWorkflow, BaseWmsService
+import parsl
 
 
 @parsl.bash_app
-def run_job(job, inputs=(), stdout=None, stderr=None):
+def run_command(command_line, inputs=(), stdout=None, stderr=None):
     """
-    Run the quantum graph associated with the specified job.
+    Run a command line as a parsl.bash_app.
     """
-    return job.command_line()
+    return command_line
 
 
 @parsl.python_app
 def no_op_job():
+    """
+    A no-op parsl.python_app to return a future for a job that already
+    has its outputs.
+    """
     return 0
 
 
@@ -30,6 +34,15 @@ class ParslJob:
     quantum graph.
     """
     def __init__(self, gwf_job, config):
+        """
+        Parameters
+        ----------
+        gwf_job: `lsst.ctrl.bps.wms.GenericWorkflowJob`
+            Workflow job containing execution information for the
+            quantum or task to be run.
+        config: `lsst.ctrl.bps.BpsConfig`
+            Configuration object with job info.
+        """
         self.gwf_job = gwf_job
         self.config = config
         self.dependencies = set()
@@ -38,19 +51,39 @@ class ParslJob:
         self.future = None
 
     def command_line(self):
+        """Return the job command line to run in bash."""
         return 'time ' + self.gwf_job.cmdline
 
     def add_dependency(self, dependency):
+        """
+        Add a job dependency based on the workflow DAG.
+
+        Parameters
+        ----------
+        dependency: `ParslJob`
+        """
         self.dependencies.add(dependency)
 
     def add_prereq(self, prereq):
+        """
+        Add a job prerequisite based on the workflow DAG.
+
+        Parameters
+        ----------
+        prereq: `ParslJob`
+        """
         self.prereqs.add(prereq)
 
     def finish(self):
+        """Set the job as done."""
         self._done = True
 
     @property
     def done(self):
+        """
+        Execution state of the job based either on runtime futures
+        or existence of outputs in the data repo.
+        """
         def future_settled(job):
             return (job.future is not None and job.future.done()
                     and job.future.exception() is None)
@@ -66,14 +99,22 @@ class ParslJob:
         return dict(stderr=os.path.join(log_dir, f'{self.gwf_job.name}.stderr'))
 
     def get_future(self):
+        """
+        Get the parsl app future for the job to be run.
+        """
         if self.done:
             self.future = no_op_job()
         if self.future is None:
             inputs = [_.get_future() for _ in self.prereqs]
-            self.future = run_job(self, inputs=inputs, **self.log_files())
+            self.future = run_command(self.command_line(), inputs=inputs,
+                                      **self.log_files())
         return self.future
 
     def have_outputs(self):
+        """
+        Use the repo butler to determine if a job's outputs are present.
+        If any outputs are missing, return False.
+        """
         butler = Butler(self.config['butlerConfig'],
                         run=self.config['outCollection'])
         registry = butler.registry
@@ -89,7 +130,20 @@ class ParslJob:
 
 
 class ParslGraph(dict):
+    """
+    Class to generate ParslJob objects with dependencies specified in
+    the generic_worklow DAG.  This class also serves as a container
+    for all of the jobs in the DAG.
+    """
     def __init__(self, generic_workflow, config):
+        """
+        Parameters
+        ----------
+        generic_workflow: `lsst.ctrl.bps.generic_workflow.GenericWorkflow`
+            Generic representation of a single workflow.
+        config: `lsst.ctrl.bps.BpsConfig`
+            Configuration of the worklow.
+        """
         super().__init__()
         self.gwf = generic_workflow
         self.config = config
@@ -97,6 +151,7 @@ class ParslGraph(dict):
         self._ingest()
 
     def _ingest(self):
+        """Ingest the workflow as ParslJobs."""
         for job_name in self.gwf:
             if job_name == 'pipetaskInit':
                 continue
@@ -105,6 +160,10 @@ class ParslGraph(dict):
                 self[successor_job].add_prereq(self[job_name])
 
     def __getitem__(self, job_name):
+        """
+        Re-implementation of dict.__getitem__ to create ParslJobs
+        from each of the named jobs in the generic_workflow.
+        """
         if not job_name in self:
             gwf_job = self.gwf.get_job(job_name)
             super().__setitem__(job_name, ParslJob(gwf_job, self.config))
@@ -112,19 +171,34 @@ class ParslGraph(dict):
 
 
 class ParslService(BaseWmsService):
-
+    """Parsl-based implementation for the WMS interface."""
     def prepare(self, config, generic_workflow, out_prefix=None):
         """
         Convert a generic workflow to a Parsl pipeline.
+
+        Parameters
+        ----------
+        config: `lss.ctrl.bps.BpsConfig`
+            Configuration of the workflow.
+        generic_workflow: `lsst.ctrl.bps.generic_workflow.GenericWorkflow`
+            Generic representation of a single workflow.
+        out_prefix : `str` [None]
+            Prefix for WMS output files.
+
+        Returns
+        -------
+        ParslWorkflow
         """
         # Import the Parsl runtime config.
         try:
-            lsst.utils.doImport(self.config['parslConfig'])
+            lsst.utils.doImport(config['parslConfig'])
         except RuntimeError:
             pass
 
-        workflow = ParslWorkflow.from_generic_workflow(config, generic_workflow,
-                                                       out_prefix)
+        service_class = 'desc.gen3_workflow.bps.wms.parsl.ParslService'
+        workflow = ParslWorkflow.\
+            from_generic_workflow(config, generic_workflow, out_prefix,
+                                  service_class)
         # Run pipetaskInit
         if workflow.parsl_graph.config['outCollection'] not in \
            workflow.parsl_graph.butler.registry.queryCollections():
@@ -137,6 +211,11 @@ class ParslService(BaseWmsService):
     def submit(self, workflow):
         """
         Submit a workflow for execution.
+
+        Parameters
+        ----------
+        workflow: `desc.gen3_workflow.bps.wms.parsl_service.ParslWorkflow`
+            Workflow object to execute.
         """
         # Request the futures of the qgraph jobs in the workflow graph
         # that have no dependencies.  These are the last jobs of the
@@ -148,13 +227,40 @@ class ParslService(BaseWmsService):
 
 
 class ParslWorkflow(BaseWmsWorkflow):
-
+    """Parsl-based workflow object to manage execution of workflow."""
     def __init__(self, name, config=None):
+        """
+        Parameters
+        ----------
+        name: `str`
+             Workflow name.
+        config: `lsst.ctrl.bps.BpsConfig`
+             Workflow config.
+        """
         super().__init__(name, config)
         self.parsl_graph = None
 
     @classmethod
-    def from_generic_workflow(cls, config, generic_workflow, out_prefix):
+    def from_generic_workflow(cls, config, generic_workflow, out_prefix,
+                              service_class):
+        """
+        Create a ParslWorkflow object from a generic_workflow.
+
+        Parameters
+        ----------
+        config: `lss.ctrl.bps.BpsConfig`
+            Configuration of the workflow.
+        generic_workflow: `lsst.ctrl.bps.generic_workflow.GenericWorkflow`
+            Generic representation of a single workflow.
+        out_prefix: `str`
+            Prefix for WMS output files.
+        service_class: `str`
+            Full module name of WMS service class that created this workflow.
+
+        Returns
+        -------
+        ParslWorkflow
+        """
         parsl_workflow = cls(generic_workflow.name, config)
         parsl_workflow.parsl_graph = ParslGraph(generic_workflow, config)
         parsl_workflow.submit_path = out_prefix
