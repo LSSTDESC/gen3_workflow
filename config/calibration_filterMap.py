f1cb12efebc3fb24d2bec8aa3ea96799c35d9335
James Chiang <jchiang87@gmail.com>
2020-12-18 12:46:48 -0800

    expand DC2_filterMap to accommodate w_2020_50 changes in filter name handling in calibration task

--- config/calibration_filterMap.py
+++ config/calibration_filterMap.py
@@ -1,3 +1,4 @@
 DC2_filterMap = {f'{_}_sim_1.4': f'lsst_{_}_smeared' for _ in 'ugrizy'}
+DC2_filterMap.update({_: f'lsst_{_}_smeared' for _ in 'ugrizy'})
 config.astromRefObjLoader.filterMap = DC2_filterMap
 config.photoRefObjLoader.filterMap = DC2_filterMap
