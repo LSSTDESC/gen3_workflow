3b05383d5c671819c06f093c599c56ee746fa3ef
Eric Charles <echarles@gmail.com>
2021-02-09 16:41:08 -0800

    Added BsubLauncher to lsfFactory

--- python/desc/gen3_workflow/bps/wms/parsl/lsfFactory.py
+++ python/desc/gen3_workflow/bps/wms/parsl/lsfFactory.py
@@ -3,8 +3,50 @@ Factory for creating HighThroughputExecutor objects for running at NERSC.
 """
 from parsl.addresses import address_by_hostname
 from parsl.executors import HighThroughputExecutor
-from parsl.launchers import WrapperLauncher
+from parsl.launchers import Launcher
 from parsl.providers import LsfProvider
+
+
+class BsubLauncher(Launcher)
+    """  Worker launcher that wraps the user's command with the Jsrun launch framework
+    to launch multiple cmd invocations in parallel on a single job allocation
+    """
+    def __init__(self, debug: bool = True, overrides: str = ''):
+        """
+        Parameters
+        ----------
+        overrides: str
+             This string will be passed to the JSrun launcher. Default: ''
+        """
+        super().__init__(debug=debug)
+        self.overrides = overrides
+
+    def __call__(self, command, tasks_per_node, nodes_per_block):
+        """
+        Args:
+        - command (string): The command string to be launched
+        - tasks_per_node (int) : Workers to launch per node
+        - nodes_per_block (int) : Number of nodes in a block
+        """
+
+        tasks_per_block = tasks_per_node * nodes_per_block
+        debug_num = int(self.debug)
+
+        x = '''set -e
+WORKERCOUNT={tasks_per_block}
+cat << JSRUN_EOF > cmd_$JOBNAME.sh
+{command}
+JSRUN_EOF
+chmod a+x cmd_$JOBNAME.sh
+bsub -n {tasks_per_block} -r {tasks_per_node} {overrides} /bin/bash cmd_$JOBNAME.sh &
+wait
+[[ "{debug}" == "1" ]] && echo "Done"
+'''.format(command=command,
+           tasks_per_block=tasks_per_block,
+           tasks_per_node=tasks_per_node,
+           overrides=self.overrides,
+           debug=debug_num)
+        return x
 
 
 HTX_OPTIONS = dict(worker_debug=False,
@@ -17,8 +59,9 @@ PROVIDER_OPTIONS = dict(nodes_per_block=1,
                         min_blocks=0,
                         max_blocks=1,
                         parallelism=0,
-                        launcher=WrappedLauncher(),
+                        launcher=BsubLauncher(),
                         cmd_timeout=300)
+
 
 
 class LsfFactory:
