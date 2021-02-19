a70094b69ba8d086593a4a62ed3af0bc2398757e
Jim Chiang <jchiang@slac.stanford.edu>
2021-02-18 21:46:09 -0800

    add skymap registration and default collection setting to conversion script; use argparse for repos command line arguments

--- scripts/gen3_setup/convert_gen2-repo.py
+++ scripts/gen3_setup/convert_gen2-repo.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
 import os
+import lsst.utils
+import lsst.daf.butler as daf_butler
 from lsst.daf.butler import Butler, DatasetType, FileDataset
 from lsst.obs.base.gen2to3 import ConvertRepoTask, RootRepoConverter, \
     CalibRepo
+from lsst.pipe.tasks.script.registerSkymap import registerSkymap
 from lsst.obs.lsst import LsstCamImSim
 
 
@@ -31,9 +35,22 @@ def makeRefCatConvertTask(butler: Butler):
 
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description=("Convert a Gen2 repo containing only raw files, "
+                     "calibs, and ref cats to Gen3"))
+    parser.add_argument('gen2_repo', type=str, help='path to Gen2 repo')
+    parser.add_argument('gen3_repo', type=str,
+                        help='empty Gen3 repo created with `butler create`')
+    args = parser.parse_args()
+
+    root2 = os.path.abspath(args.gen2_repo)
+    root3 = os.path.abspath(args.gen3_repo)
+    if not os.path.isdir(root3):
+        raise RuntimeError(f'{args.gen3_repo} does not exist.  '
+                           f'Create it with `butler create ...`')
+
     instrument = LsstCamImSim()
-    root2 = 'gen2-repo'
-    root3 = 'gen3-repo'
 
     # Convert raw files and CALIBs
     fresh_start = True
@@ -54,3 +71,25 @@ if __name__ == '__main__':
                               universe=butler.registry.dimensions)
     butler.registry.registerDatasetType(datasetType)
     butler.ingest(*listOfFileDataset)
+
+    # Register the DC2 skymap.
+    config = None
+    config_file = os.path.join(lsst.utils.getPackageDir('gen3_workflow'),
+                               'config', 'makeSkyMap.py')
+    registerSkymap(root3, config, config_file)
+
+    # Set the default collection to 'LSSTCam-imSim/defaults'.
+    butler = Butler(root3, writeable=True)
+    registry = butler.registry
+
+    default_collection = 'LSSTCam-imSim/defaults'
+
+    if default_collection in list(registry.queryCollections()):
+        registry.removeCollection(default_collection)
+
+        registry.registerCollection(default_collection,
+                                    type=daf_butler.CollectionType.CHAINED)
+        registry.setCollectionChain(default_collection,
+                                    ['LSSTCam-imSim/raw/all',
+                                     'LSSTCam-imSim/calib',
+                                     'skymaps', 'ref_cat'])
