c1abf73932bdff6e2305b4958ded6f45e4d59d0d
Quentin Le Boulc'h <quentin.leboulch@cc.in2p3.fr>
2022-03-28 16:56:01 +0200

    Update bash apps following slurm_config change

--- python/desc/gen3_workflow/bash_apps.py
+++ python/desc/gen3_workflow/bash_apps.py
@@ -1,6 +1,6 @@
 import parsl
 
-__all__ = ['bash_app_3G', 'bash_app_9G', 'bash_app_18G', 'bash_app_48G', 'bash_app_120G',
+__all__ = ['bash_app_3G', 'bash_app_6G', 'bash_app_18G', 'bash_app_54G', 'bash_app_120G',
            'local_bash_app']
 
 ignore_for_cache = ['stdout', 'stderr']
@@ -9,13 +9,13 @@ ignore_for_cache = ['stdout', 'stderr']
 bash_app_3G = parsl.bash_app(executors=['batch-3G'], cache=True,
                                 ignore_for_cache=ignore_for_cache)
 
-bash_app_9G = parsl.bash_app(executors=['batch-9G'], cache=True,
+bash_app_6G = parsl.bash_app(executors=['batch-6G'], cache=True,
                                 ignore_for_cache=ignore_for_cache)
 
 bash_app_18G = parsl.bash_app(executors=['batch-18G'], cache=True,
                                 ignore_for_cache=ignore_for_cache)
 
-bash_app_48G = parsl.bash_app(executors=['batch-48G'], cache=True,
+bash_app_54G = parsl.bash_app(executors=['batch-54G'], cache=True,
                                 ignore_for_cache=ignore_for_cache)
 
 bash_app_120G = parsl.bash_app(executors=['batch-120G'], cache=True,
